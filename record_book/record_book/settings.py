63b629c54af38e6baeb96778a498dfa47a7e39c6
Oleg <oleg20064@gmail.com>
2020-10-25 17:05:36 +0300

    Сверстал первые страницы. Разбил верстку на расширяемые блоки внутри основной модели. Настроил БД по схеме(думаю, что все верно). Далее нужно доверстать оставшиеся страницы, сделать вывод данных из бд и в идеале сделать систему регистрации.

--- record_book/record_book/settings.py
+++ record_book/record_book/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'first_page',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -80,9 +79,6 @@ DATABASES = {
     }
 }
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -116,6 +112,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
